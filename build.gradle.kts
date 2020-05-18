4f7b50680976f8b1c35fb2323e23bb0e53202b58
Lundez <hampus.londogard@gmail.com>
2020-05-18 16:55:29 +0200

    Added GH Actions & updated gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    java
+    `maven-publish`
+    id("org.jetbrains.dokka") version "0.9.17"
     kotlin("jvm") version "1.3.72"
 }
 
 group = "com.londogard"
 version = "1.0-SNAPSHOT"
-val kluentVersion = 1.61
+val kluentVersion = "1.61"
+val smileVersion = "2.4.0"
 
 repositories {
     jcenter()
@@ -17,23 +22,47 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("com.github.haifengl:smile-nlp:2.4.0")
-    implementation("com.github.haifengl:smile-core:2.4.0")
-    implementation("com.github.haifengl:smile-kotlin:2.4.0")
+    implementation("com.github.haifengl:smile-nlp:$smileVersion")
+    implementation("com.github.haifengl:smile-core:$smileVersion")
+    implementation("com.github.haifengl:smile-kotlin:$smileVersion")
 
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.72")
-    // testImplementation("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(tasks.dokka)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/londogard/embeddings-kt")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
     }
 }
--- src/main/kotlin/com/londogard/embeddings/NormAvgSentenceEmbeddings.kt
+++ src/main/kotlin/com/londogard/embeddings/NormAvgSentenceEmbeddings.kt
@@ -2,5 +2,8 @@ package com.londogard.embeddings
 
 
 class NormAvgSentenceEmbeddings(val embeddings: Embeddings) : SentenceEmbeddings {
-    override fun getSentenceEmbedding(tokens: List<String>): Array<Float> = TODO()
+    override fun getSentenceEmbedding(tokens: List<String>): Array<Float> = tokens
+            .mapNotNull(embeddings::vector)
+            .sumByColumns()
+            .normalize()
 }
